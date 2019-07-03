bd838712e3a8b1cb37b25325e45a83383f840f84
gabora <gabor.attila87@gmail.com>
2019-07-03 11:27:59 +0200

    export edge strength for best model found during mfu sampling

--- drug_prediction/export_features.R
+++ drug_prediction/export_features.R
@@ -52,297 +52,61 @@ write_rds(feature_table_1_2, path = paste0(feature_folder,"/ft_2_raw_parameters_
 
 
 ### Model feautures 2.2 : timecourse edge strenght -----------------------------
-source("../KinaseLibraryScan/R/getReactionActivity.R")
-
-if(FALSE){
-	timecourse_strength_to_feature = function(model){
-		reacData = getReactionActivity_logicModel(self = model)
-		return(reacData$timecourse)
-	}
-
-	feature_table_2_2 = ldply(calibrated_models,function(M){
-		features = timecourse_strength_to_feature(model = M)
-	},.id = "cell_line" ,.progress = progress_text())
+source("./drug_prediction/getReactionActivity.R")
+base_time = c(0,5.5,7,9,13,17,23,30,40,60)
 
 
-	feature_table_2_2_timecourse_edge_strength = dcast(feature_table_2_2,cell_line+exp+time+modelName~reacID,value.var = "y")
-	# remove the control exp
-	feature_table_2_2_timecourse_edge_strength = feature_table_2_2_timecourse_edge_strength[feature_table_2_2_timecourse_edge_strength$exp != "exp 1",]
+# update the calibrated models with the best parameters found
+mfu_parameters <- read_rds('./data/models/pkn_v4_midas_v4/parameter_samplings_QC.RDS') %>% as_tibble()
 
-	save(feature_table_2_2_timecourse_edge_strength,file=paste0(feature_folder,"/feature_table_2_time_course_activity.RData"))
-}else{
-
-	# import and interpolate for all timepoints
-	load(file=paste0(feature_folder,"/feature_table_2_time_course_activity.RData"))
-	# load variable feature_table_2_2_timecourse_edge_strength
-}
-head(feature_table_2_2_timecourse_edge_strength)
-
-colnames(feature_table_2_2_timecourse_edge_strength)[[2]] = "treatment"
-feature_table_2_2_timecourse_edge_strength$treatment = as.character(feature_table_2_2_timecourse_edge_strength$treatment)
-feature_table_2_2_timecourse_edge_strength$treatment[feature_table_2_2_timecourse_edge_strength$treatment == "exp 2"] = "EGF"
-feature_table_2_2_timecourse_edge_strength$treatment[feature_table_2_2_timecourse_edge_strength$treatment == "exp 3"] = "iEGFR"
-feature_table_2_2_timecourse_edge_strength$treatment[feature_table_2_2_timecourse_edge_strength$treatment == "exp 4"] = "iMEK"
-feature_table_2_2_timecourse_edge_strength$treatment[feature_table_2_2_timecourse_edge_strength$treatment == "exp 5"] = "imTOR"
-feature_table_2_2_timecourse_edge_strength$treatment[feature_table_2_2_timecourse_edge_strength$treatment == "exp 6"] = "iPI3K"
-feature_table_2_2_timecourse_edge_strength$treatment[feature_table_2_2_timecourse_edge_strength$treatment == "exp 7"] = "iPKC"
-
-# summary of features:
-
-unique_conds = unique(feature_table_2_2_timecourse_edge_strength[,c("treatment","time","cell_line")])
-
-condition_time_dist = dcast(unique_conds,treatment~time,value.var = "cell_line")
-write.csv(condition_time_dist,file.path(feature_folder,"condition_time_dist_before.csv"))
-
-if(FALSE){
-	# produce feature from each cell line for each time point:
-	unique_time = unique(feature_table_2_2_timecourse_edge_strength$time)
-
-	feature_cols = grep("=", colnames(feature_table_2_2_timecourse_edge_strength))
-
-	feature_table_2_3_full_timecourse_edge_strength = ddply(feature_table_2_2_timecourse_edge_strength,.(cell_line,exp),function(df){
-		# df = filter(feature_table_2_3_full_timecourse_edge_strength,cell_line=="184A1",exp=="exp 3")
-		# approx(x=df$time,y = df$`PIP3=AKT_S473`,xout = unique_time)
-		interp_matrix = apply(df[,feature_cols],2,function(col_dat){approx(x = df$time,y=col_dat,xout = unique_time,method = "linear",rule = 2)[['y']]})
-		cbind(time=unique_time,interp_matrix)
-	})
-
-	# exp 1 — not included
-	# exp 2 —  EGF+SERUM
-	# exp 3 — iEGFR
-	# exp 4 — iMEK
-	# exp 5 — imTOR
-	# exp 6 — iPI3K
-	# exp 7 — iPKC
-	colnames(feature_table_2_3_full_timecourse_edge_strength)[[2]] = "treatment"
-	feature_table_2_3_full_timecourse_edge_strength$treatment = as.character(feature_table_2_3_full_timecourse_edge_strength$treatment)
-	feature_table_2_3_full_timecourse_edge_strength$treatment[feature_table_2_3_full_timecourse_edge_strength$treatment == "exp 2"] = "EGF"
-	feature_table_2_3_full_timecourse_edge_strength$treatment[feature_table_2_3_full_timecourse_edge_strength$treatment == "exp 3"] = "iEGFR"
-	feature_table_2_3_full_timecourse_edge_strength$treatment[feature_table_2_3_full_timecourse_edge_strength$treatment == "exp 4"] = "iMEK"
-	feature_table_2_3_full_timecourse_edge_strength$treatment[feature_table_2_3_full_timecourse_edge_strength$treatment == "exp 5"] = "imTOR"
-	feature_table_2_3_full_timecourse_edge_strength$treatment[feature_table_2_3_full_timecourse_edge_strength$treatment == "exp 6"] = "iPI3K"
-	feature_table_2_3_full_timecourse_edge_strength$treatment[feature_table_2_3_full_timecourse_edge_strength$treatment == "exp 7"] = "iPKC"
-	head(feature_table_2_3_full_timecourse_edge_strength)
-
-	saveRDS(feature_table_2_3_full_timecourse_edge_strength,file=paste0(feature_folder,"/feature_table_2_3_full_timecourse_edge_strength.RDS"))
-
-	GDSC_cell_lines = colnames(read.csv("data/DATA_GDSC/GDSC_IC50_breast.csv"))
-	feature_table_2_3_full_timecourse_edge_strength_GDSC_cellines = filter(feature_table_2_3_full_timecourse_edge_strength,cell_line %in% GDSC_cell_lines)
-	feature_table_2_3_full_timecourse_edge_strength_GDSC_cellines = filter(feature_table_2_3_full_timecourse_edge_strength,cell_line %in% GDSC_cell_lines, time %in% c(0,5.5,7,9,13,17,23,30,40,60))
-	saveRDS(feature_table_2_3_full_timecourse_edge_strength_GDSC_cellines,file=paste0(feature_folder,"/feature_table_2_3_full_timecourse_edge_strength_GDSC_cellines.RDS"))
-}else{
-	feature_table_2_3_full_timecourse_edge_strength = readRDS(file=paste0(feature_folder,"/feature_table_2_3_full_timecourse_edge_strength.RDS"))
-
-}
-unique_conds = unique(feature_table_2_3_full_timecourse_edge_strength[,c("treatment","time","cell_line")])
-unique_conds = unique(feature_table_2_3_full_timecourse_edge_strength_GDSC_cellines[,c("treatment","time","cell_line")])
-
-condition_time_dist = dcast(unique_conds,treatment~time,value.var = "cell_line")
+# find the parameters with smallest fobj for each cell-line.
+feature_table_1_2 = mfu_parameters %>%
+	group_by(cell_line) %>% top_n(1,-fobj) %>%
+	select(cell_line,everything(),-fobj,-rel_fobj,-sampling_id,-run_id,-starts_with("x0"))
 
 
-
-
-ggplot(filter(feature_table_2_3_full_timecourse_edge_strength,cell_line=="184A1")) + geom_line(aes(time,`ERK12=p90RSK` ,col=treatment))
-
-###  Model features 3 ---------------------------------------------------------
-# measured and predicted states at iPI3K, timepoint 13
-iexp_iPI3K = 5
-predicted_nodes_ipi3k_time13 = ldply(calibrated_models,function(M){
-	s = M$simulateStates()
-	time_13 = which(s$timepoints==13)
-	if(length(time_13)== 0) {
-		print(paste("time13_missing ", M$exps$name))
-		# then take the closest measured time
-		time_13 = which.min(abs(s$timepoints - 13))
-	}
-
-	s$signals[[time_13]][iexp_iPI3K,]
-
-},.id = "cell_line")
-
-
-MIDAS_files = list.files("data/MIDAS_v2/",pattern = ".csv",full.names = T)
-cellline_names = gsub(".csv","",basename(MIDAS_files),fixed = T)
-MIDAS_files = as.list(MIDAS_files)
-names(MIDAS_files) = cellline_names
-
-# i = 1
-measured_nodes_ipi3k_time13  =  ldply(MIDAS_files,function(m){
-	s = CNOlist$new(m,verbose = F)
-
-	time_13 = which(s$timepoints==13)
-	if(length(time_13)== 0) {
-		print(paste("time13_missing ", M$exps$name))
-		# then take the closest measured time
-		time_13 = which.min(abs(s$timepoints - 13))
-	}
-
-	s$signals[[time_13]][iexp_iPI3K,]
-
-
-},.id = "cell_line")
-
-
-# merge the measured and the predicted states:
-# order the same way
-measured_nodes_ipi3k_time13 = measured_nodes_ipi3k_time13[order(measured_nodes_ipi3k_time13$cell_line), ]
-predicted_nodes_ipi3k_time13 = predicted_nodes_ipi3k_time13[order(predicted_nodes_ipi3k_time13$cell_line),]
-
-# get the nodes from the models that were not measured, for other nodes we use the scaled values:
-predicted_nodes_only_iPI3K_time13 = predicted_nodes_ipi3k_time13[,!colnames(predicted_nodes_ipi3k_time13) %in% colnames(measured_nodes_ipi3k_time13)]
-
-all_measured_and_predicted_iPI3K_time13 = cbind(measured_nodes_ipi3k_time13,predicted_nodes_only_iPI3K_time13)
-
-save(all_measured_and_predicted_iPI3K_time13,file="all_measured_and_predicted_iPI3K_time13.RData")
-
-
-
-# get all the predicted values plus the measured values for the nodes that are not modelled:
-measured_nodes_only_iPI3K_time13 = measured_nodes_ipi3k_time13[,!colnames(measured_nodes_ipi3k_time13) %in% colnames(predicted_nodes_ipi3k_time13)]
-
-
-measured_and_all_predicted_iPI3K_time13 =  cbind(predicted_nodes_ipi3k_time13,measured_nodes_only_iPI3K_time13)
-
-save(measured_and_all_predicted_iPI3K_time13,file="measured_and_all_predicted_iPI3K_time13.RData")
-
-
-
-### Plotting model features on the graph ---------------------------------------
-source("./R/plotAnnotatedModel.R")
-# my_color_scale = rev(c("#B2182B", RColorBrewer::brewer.pal(7,"RdBu"),"#2166AC"))
-my_color_scale = RColorBrewer::brewer.pal(9,"YlGnBu")
-
-num_2_my_color_scale = colorRamp(my_color_scale)
-num_2_my_color_scale = colorRamp(c('#AAAAAA','#FF1111'))
-
-
-reac_colors = rgb(num_2_my_color_scale(feature_table_2[1,2:ncol(feature_table_2)]),maxColorValue = 255)
-names(reac_colors) = colnames(feature_table_2)[2:ncol(feature_table_2)]
-
-plotAnnotatedModel(model = calibrated_models[[1]]$pkn$network,
-				   CNOlist = calibrated_models[[1]]$exps$convertToS4(),
-				   annotEdgeColors = reac_colors,annotEdgeMatching = 'reacID')
-
-pdf("figures/fitted_models_plotFit.pdf",width = 21,height = 3)
-l_ply(seq_along(calibrated_models),function(i,M){
-	M[[i]]$plotFit(measuredNodesOnly = F)
-	title(paste0(i,"-",M[[i]]$exps$name))
-},M = calibrated_models,.progress = progress_text())
-dev.off()
-
-pdf("figures/fitted_models_plotAnnotatedModel_features_2.pdf",width = 8,height = 8)
-l_ply(seq_along(calibrated_models),function(i,M){
-
-	reac_colors = rgb(num_2_my_color_scale(feature_table_2[i,2:ncol(feature_table_2)]),maxColorValue = 255)
-	names(reac_colors) = colnames(feature_table_2)[2:ncol(feature_table_2)]
-
-	plotAnnotatedModel(model = calibrated_models[[i]]$pkn$network,
-					   CNOlist = calibrated_models[[i]]$exps$convertToS4(),
-					   annotEdgeColors = reac_colors,annotEdgeMatching = 'reacID')
-
-	title(paste0(i,"-",gsub(".csv","",M[[i]]$exps$name,fixed = T)))
-},M = calibrated_models,.progress = progress_text())
-dev.off()
-
-
-###### plot PKN with gggraph
-#
-# library(ggraph)
-# #> Loading required package: ggplot2
-# library(tidygraph)
-# library(igraph)
-#
-# sif = read.table("./data/pkn/cancer_cellLines_v2.sif")
-# #sif = unique(sif)
-# #write.table(sif,"./data/pkn/cancer_cellLines_v3.sif",quote = F,sep = '\t',col.names = F,row.names = F)
-#
-# graph = as_tbl_graph(sif[,c(1,3)])
-# E(graph)$sign = sif[,2]
-# ggraph::create_layout(g = graph,layout = 'graphopt')
-# ggraph(graph, layout = 'graphopt') + # 'kk'
-# 	geom_edge_fan(aes(start_cap = label_rect(node1.name),
-# 					   end_cap = label_rect(node2.name)),
-# 				   arrow = arrow(length = unit(4, 'mm'))) +
-# 	geom_node_text(aes(label=name))+
-# 	theme_graph(foreground = 'steelblue', fg_text_colour = 'white')
-#
-#
-#
-# G = igraph::graph_from_data_frame(sif[,c(1,3)])
-# igraph::plot.igraph(G)
-
-
-
-# computes the AUC of the combined transfer functions described in the reacID over the time.
-# then maps this values to color scale
-transfer_functions_to_edge_colors = function(model,stat=c("meanActivity","AUC")){
-	source("R/getReactionActivity.R")
-	stat = match.arg(stat)
-	reacData = getReactionActivity_logicModel(self = model,timeSignals = seq(0,600,by=60) )
-
-	k2color <- colorRamp(RColorBrewer::brewer.pal(7,"Greys"))
-
-	edge_value = reacData$summary[reacData$summary$exp=="exp 3",stat]
-	names(edge_value) = reacData$summary[reacData$summary$exp=="exp 3","reacID"]
-
-	edge_colors = rgb(k2color(edge_value/max(edge_value)),maxColorValue = 255)
-	names(edge_colors) = names(edge_value)
-	return(list(edge_colors = edge_colors,
-				edge_values = edge_value))
+timecourse_strength_to_feature = function(model){
+	reacData = getReactionActivity_logicModel(logicODEmodel = model,timeSignals = base_time)
+	return(reacData$timecourse)
 }
 
+feature_table_2_2 = ldply(calibrated_models,function(M){
 
-node_activation_to_node_colors = function(model, type=c("control_to_low","low_to_high")){
+	mfu_min_fobj <- mfu_parameters %>% filter(cell_line == M$name) %>% pull(fobj) %>% min()
+	if (M$getStatistics()$fObj > mfu_min_fobj){
 
-	type = match.arg(type)
+		best_vec = mfu_parameters %>% filter(cell_line == M$name) %>%  # find the cell-line
+			arrange(fobj) %>% slice(1) %>% # sleect the row with smallest objective function value
+			select(everything(),-cell_line, -fobj,-rel_fobj,-sampling_id,-run_id)
 
-	S = model$fillCueValuesInSimulation()
+		best_vec_pars <- best_vec %>% select(-starts_with("x0"))
+		best_vec_x0 <- best_vec %>% select(starts_with("x0"))
 
-	if(type=="control_to_low"){
-		# effect = Signal_low_time2 - Signal_control_time2
-		effect = S[[2]][2,] - S[[2]][1,]
-	}else if(type=="low_to_high"){
-		# effect = Signal_high_time2 - Signal_low_time2
-		effect = S[[2]][3,] - S[[2]][2,]
-	}
-	# the effect is \in [-1, 1], therefore
-	# effect/2 + 0.5 is \in [0,1] and 0.5 corresponds to the unscaled_effect == 0
+		stopifnot(all(M$ode_parameters$parNames[M$ode_parameters$index_opt_pars]==names(best_vec_pars)))
 
+		M$ode_parameters$parValues[M$ode_parameters$index_opt_pars] = best_vec_pars %>% flatten_dbl()
 
-	effect2color <- colorRamp(effect2node_colors)
+		M$ode_parameters$x0Values[M$ode_parameters$index_opt_x0] = best_vec_x0 %>% flatten_dbl()
 
-	node_colors = rgb(effect2color(0.5+0.5*effect),maxColorValue = 255)
-	names(node_colors) = names(effect)
-	return(list(node_colors = node_colors,
-				node_values = effect))
-}
-
-transfer_functions_to_bitstring = function(model,reacID_values,threshold=0.01, tau_threshold=1e-8){
-
-
-	# weight the edge_values with the downstream node's tau parameter
-	tau_pars = model$ode_parameters$parValues[model$ode_parameters$index_tau]
-	reacs = names(reacID_values)
-
-	get_downstream_node_name = function(reacID){
-		strsplit(reacID,split = "=")[[1]][[2]]
-	}
-	#i=1
-	for(i in seq_along(reacs)){
-		outnode = get_downstream_node_name(reacs[[i]])
-		outnode_tau = tau_pars[paste0("tau_",outnode)]
-		if(outnode_tau <= tau_threshold){
-			reacID_values[reacs[i]] = 0
-		}
+		M$updateInitialValueEstimates(ode_parameters = M$ode_parameters,
+									  initialConditions = M$initialConditions)
 	}
 
-	bitString = as.logical(reacID_values>threshold)
-	names(bitString) = names(reacID_values)
-	return(bitString)
-}
+	features = timecourse_strength_to_feature(model = M)
+},.id = "cell_line" ,.progress = progress_text())
+
+feature_table_2_2 <- feature_table_2_2 %>% as_tibble() %>% spread(reacID,y) %>%
+	filter(exp !="exp 1") %>% mutate(exp = as.character(exp)) %>%
+	mutate(exp = case_when(
+		exp == "exp 2" ~ "EGF",
+		exp == "exp 3" ~ "iEGFR",
+		exp == "exp 4" ~ "iMEK",
+		exp == "exp 5" ~ "imTOR",
+		exp == "exp 6" ~ "iPI3K",
+		exp == "exp 7" ~ "iPKC")) %>% select(-modelName)
+
+write_rds(feature_table_2_2, path = paste0(feature_folder,"/ft_3_edge_strength.rds"))
 
 
-source("../KinaseLibraryScan/R/plotAnnotatedModel.R")
-plotAnnotatedModel()
 
 
